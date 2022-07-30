ef84571aee9fb49b8f895e4b3057b271282ec67f
Madiyar Aitbayev <maitbayev@snapchat.com>
2022-07-30 19:46:34 +0100

    Try to fix for windows

--- build.rs
+++ build.rs
@@ -7,12 +7,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     let dst = Config::new("libprimecount")
         .define("BUILD_PRIMECOUNT", "OFF")
         .build();
-
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let target_env = env::var("CARGO_CFG_TARGET_ENV").unwrap();
     // now - emitting some cargo commands to build and link the lib
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
-    match env::var("TARGET")? {
-        t if t.contains("apple") => println!("cargo:rustc-link-lib=dylib=c++"),
-        t if t.contains("linux") => println!("cargo:rustc-link-lib=dylib=stdc++"),
+    match (target_os.as_str(), target_env.as_str()) {
+        ("linux", _) => println!("cargo:rustc-link-lib=dylib=stdc++"),
+        ("macos", _) | ("ios", _) => println!("cargo:rustc-link-lib=dylib=c++"),
+        ("windows", "msvc") => (),
+        ("windows", "gnu") => println!("cargo:rustc-link-lib=dylib=stdc++"),
         _ => unimplemented!(),
     }
     println!("cargo:rustc-link-lib=static=primecount");
